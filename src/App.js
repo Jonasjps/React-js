79c844c2fbde79d937c291268bc45d9e17fccf1f
Jonas Pessoa <Jonaspsjw@hotmail.com>
2024-03-02 21:00:58 -0300

    Atualizando app react-js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+const BemVindo = props => {
+    return(
+        <div>
+            <h2>Bem-Vindo(a): {props.name}</h2>
+            <h3>Tenho {props.age} anos </h3>
+        </div>
+    );
+}
+
 function App () {
     return (
         <div>
             <h1>Social Midea</h1>
+            <BemVindo name="Jonas" age="28" />
+            <BemVindo name="Virginia" age="30" />
+            <p>Estou aprendsendo a desenvolver com react-js</p>
         </div>
     );
 }
